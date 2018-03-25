e660409effefcf1419f20c6215ef001aa9f73bd8
maryhoag <marymcevoyhoag@gmail.com>
2018-03-25 11:47:34 -0400

    typo fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore()
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+	<Provider store={store}	
+		<App />
+	</Provider>,
+	document.getElementById('root'));
+	
 registerServiceWorker();
 
 
